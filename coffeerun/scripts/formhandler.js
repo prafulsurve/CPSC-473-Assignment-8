4d47b7f73157dc7fe3f8047d844cacde8544f59e
Praful Surve <survepraful@live.com>
2017-03-22 20:34:52 -0700

    Chapter 14

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -25,9 +25,11 @@
             });
 
             console.log(data);
-            fn(data);
-            this.reset();
-            this.elements[0].focus();
+            fn(data)
+            .then(function () {
+                this.reset();
+                this.elements[0].focus();
+            }.bind(this));
         });
     };
 
